cd5bc0803bbbdd9e7afdef6e25b1e1cac6058772
Christopher M. Judd <javajudd@gmail.com>
2023-08-28 20:46:58 -0400

    Added Simple Streaming Word Count example(s).

---
+++ src/main/scala/net/javajudd/spark/streams/WordCountStream.scala
@@ -0,0 +1,22 @@
+package net.javajudd.spark.streams
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+// nc -lk 9999
+object WordCountStream {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .setAppName("WorkCountStream")
+
+    val ssc = new StreamingContext(conf, Seconds(1))
+    ssc.socketTextStream("localhost", 9999)
+      .flatMap(_.split(" "))
+      .map(w => (w, 1))
+      .reduceByKey(_ + _)
+      .print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
