5da395ad5ba006eb5dd1ece3f0b9ba8630ec1bcb
Christopher M. Judd <javajudd@gmail.com>
2023-10-10 15:53:14 -0400

    Added explain plan & partition counts DAG example(s).

--- src/main/scala/net/javajudd/spark/dag/DAGExample.scala
+++ src/main/scala/net/javajudd/spark/dag/DAGExample.scala
@@ -9,12 +9,20 @@ object DAGExample {
       .getOrCreate()
 
     val ds1 = spark.range(1, 10000000)
+    println(s"ds1 partition size: ${ds1.rdd.partitions.size}") // 16
     val ds2 = spark.range(1, 10000000, 2)
+    println(s"ds2 partition size: ${ds2.rdd.partitions.size}") // 16
     val ds3 = ds1.repartition(7)
+    println(s"ds3 partition size: ${ds3.rdd.partitions.size}") // 7
     val ds4 = ds2.repartition(9)
+    println(s"ds4 partition size: ${ds4.rdd.partitions.size}") // 9
     val ds5 = ds3.selectExpr("id * 5 as id")
+    println(s"ds5 partition size: ${ds5.rdd.partitions.size}") // 7
     val joined = ds5.join(ds4, "id")
+    println(s"joined partition size: ${joined.rdd.partitions.size}") // 200
     val sum = joined.selectExpr("sum(id)")
+    println(s"sum partition size: ${sum.rdd.partitions.size}") // 1
     sum.show()
+    sum.explain()
   }
 }
